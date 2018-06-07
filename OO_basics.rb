11309795594967593d5e3617792ee4791deb5862
valeria farfan <valeria073104@gmail.com>
2018-06-07 20:42:33 +0000

    automatically backed up by learn

--- OO_basics.rb
+++ OO_basics.rb
@@ -1,42 +1,47 @@
 class Book
  
+ attr_accessor :title, :author, :page_count, :genre
+ 
   def initalize(title, author, page_content, genre)
     @title = title
-    @author = author
-    @page_content = page_content
-    @genre = genre
   end
   
-  def title
-    @title
-  end
+#   def title
+#     @title
+#   end
   
-  def title=(title)
-    @title = title
-  end
+#   def title=(title)
+#     @title=title
+#   end
   
-  def author
-    @author
-  end
+#   def author
+#     @author
+#   end
   
-  def author=(author)
-   @author = author
- end
+#   def author=(author)
+#   @author=author
+# end
  
- def page_content
-   @page_content
- end
+# def page_count
+#   @page_count
+# end
  
- def page_content=(page_content)
-   @page_content = page_content
- end
+# def page_count=(page_count)
+#   @page_count=page_count
+# end
  
- def genre 
-   @genre
-   end
+# def genre 
+#   @genre
+# end
    
-   def genre=(genre)
-   @genre = genre
- end
+# def genre=(genre)
+#   @genre=genre
+# end
+
+end
+
+book1 = Book.new("catching fire")
+book1.author
+ 
  
  
